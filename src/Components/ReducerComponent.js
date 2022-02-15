745f8471ae7b109d5f8098a268b137c56bbe3025
Jonathan García <megabyte9703@gmail.com>
2022-02-14 21:13:24 -0600

    Cambiando el switch por un objeto de funciónes, y evitamos retornar el dispatch en el hook

--- src/Components/ReducerComponent.js
+++ src/Components/ReducerComponent.js
@@ -3,7 +3,7 @@ import useReducerHook from '../hooks/useReducerHook'
 
 export default function ReducerComponent() {
   const [name, setName] = useState('')
-  const { state, dispatch, actions } = useReducerHook()
+  const { state, actions, changeName, deleteLastName } = useReducerHook()
 
   const handleChangeName = (e) => {
     setName(e.target.value)
@@ -22,12 +22,12 @@ export default function ReducerComponent() {
         onChange={handleChangeName}
       />
       <button
-        onClick={() => dispatch({ type: actions.CHANGE_NAME, payload: name })}
+        onClick={() => changeName({ type: actions.CHANGE_NAME, payload: name })}
       >
         Change Name
       </button>
       <br />
-      <button onClick={() => dispatch({ type: actions.DELETE_LASTNAME })}>
+      <button onClick={() => deleteLastName({ type: actions.DELETE_LASTNAME })}>
         Delete last name
       </button>
     </>
--- src/hooks/useReducerHook.js
+++ src/hooks/useReducerHook.js
@@ -1,45 +1,53 @@
 import { useReducer } from 'react'
 
 const ACTIONS = {
-  //CONSTANTE CON NOMBRES POSIBLES PARA LOS ACTION.PAYLOAD
   CHANGE_NAME: 'change_name',
   DELETE_LASTNAME: 'delete_lastname'
 }
 
 const initialState = {
-  //ESTADO INICIAL DEL REDUCER, VALORES INICIALES
   name: 'Jonathan',
   lastName: 'García'
 }
 
-const reducer = (state, action) => {
-  //DEFINIMOS LO QUE HARA SEGÚN EL ACTION.PAYLOAD
-  //AL DAR UN OBJETO COMO PARAMETRO AL DISPATCH, SUS ELEMENTOS VENDRÁN EN EL ACTION
-  switch (action.type) {
-    case ACTIONS.DELETE_LASTNAME:
-      return {
-        ...state,
-        lastName: null
-      }
-    case ACTIONS.CHANGE_NAME:
-      return {
-        ...state,
-        name: action.payload
-        //EN CASO DE REQUERIR UN PAYLOAD, LO SACAMOS TAMBIEN DEL OBJETO DEL DISPATCH
-      }
-    default:
-      return state
+const ACTIONS_REDUCERS = {
+  //EN LUGAR DEL SWITCH TENDREMOS UN OBJ DE FUNCIONES
+  //CADA ELEMENTO TENDRÁ COMO NOMBRE CADA ACTION, Y COMO VALOR UNA FUNCIÓN CON EL STATE Y EL ACTION
+  //CADA FUNCIÓN HARA LO QUE SE HACIA EN CADA CASO DEL SWITCH
+
+  [ACTIONS.CHANGE_NAME]: (state, action) => {
+    return {
+      ...state,
+      name: action.payload
+    }
+  },
+  [ACTIONS.DELETE_LASTNAME]: (state, action) => {
+    return {
+      ...state,
+      lastName: null
+    }
   }
+}
+
+const reducer = (state, action) => {
+  //EL REDUCER LLAMARÁ A ALGUNA DE LAS FUNCIONES DEL OBJETO ANTERIOR, DEPENDIENDO DE SU ACTION.TYPE
+  //RETORNAREMOS EL ESTADO SÍ LO ANTERIOR NO TIENE UN VALOR, SÍ NO EXISTE NIGUNA FUNCIÓN CON EL TYPE INGRESADO
+  //DE LO CONTRARIO, EJECUTAMOS LA FUNCIÓN TRAÍDA, DANDOLE POR PARAMETRO EL STATE Y EL ACTION
+  const actionsReducer = ACTIONS_REDUCERS[action.type]
+
+  return actionsReducer ? actionsReducer(state, action) : state
 }
 
 export default function useReducerHook() {
   const [state, dispatch] = useReducer(reducer, initialState)
-  //EL STATE TIENE LOS DATOS A USAR, LOS INICIALES SON LOS QUE SE COLOQUEN AL useReducer COMO 2DO PARAM.
-  //EL DISPATCH ES UN METODO QUE SE USARÁ PARA ACTIVAR LOS ACTIONS
 
   return {
     state,
-    dispatch,
-    actions: ACTIONS
+    actions: ACTIONS,
+    changeName: ({ payload }) =>
+      dispatch({ type: ACTIONS.CHANGE_NAME, payload }),
+    deleteLastName: () => dispatch({ type: ACTIONS.DELETE_LASTNAME })
   }
+
+  //ADICIONALMENTE, YA NO RETORNAMOS TODO EL DISPATCH, SINO FUNCIÓNES QUE EJECUTAN EL DISPATCH, Y QUE LLEVEN EL PAYLOAD POR PARAMETRO
 }
